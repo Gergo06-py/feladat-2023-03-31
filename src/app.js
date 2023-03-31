b8be0f2796b06d93015c0650a709cc6d70ecf07c
Gergo06 <58563706+Gergo06-py@users.noreply.github.com>
2023-03-31 08:25:17 +0000

    placed some semicolons

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const doc = {
-    elsoTelekHossza: document.querySelector("#elsoTelekHossza"),
-    masodikTelekHossza: document.querySelector("#masodikTelekHossza"),
-    result: document.querySelector("#result")
-}
+	elsoTelekHossza: document.querySelector("#elsoTelekHossza"),
+	masodikTelekHossza: document.querySelector("#masodikTelekHossza"),
+	result: document.querySelector("#result"),
+};
 
 function calcFullArea() {
-    let fullArea = calcArea(Number(doc.elsoTelekHossza.value)) + calcArea(Number(doc.masodikTelekHossza.value)) / 2;
-    doc.result.value = fullArea;
+	let fullArea =
+		calcArea(Number(doc.elsoTelekHossza.value)) +
+		calcArea(Number(doc.masodikTelekHossza.value)) / 2;
+	doc.result.value = fullArea;
 }
 
 function calcArea(side) {
-    area = (3 / 2) * Math.pow(side, 2) * Math.sqrt(3)
-    return area
+	area = (3 / 2) * Math.pow(side, 2) * Math.sqrt(3);
+	return area;
 }
