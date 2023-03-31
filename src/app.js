055d127ec66fbbff966e4aca29cec8ad630b9ca4
Gergo06 <58563706+Gergo06-py@users.noreply.github.com>
2023-03-31 08:20:48 +0000

    Calculation done

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const doc = {
+    elsoTelekHossza: document.querySelector("#elsoTelekHossza"),
+    masodikTelekHossza: document.querySelector("#masodikTelekHossza"),
+    result: document.querySelector("#result")
+}
+
+function calcFullArea() {
+    let fullArea = calcArea(Number(doc.elsoTelekHossza.value)) + calcArea(Number(doc.masodikTelekHossza.value)) / 2;
+    doc.result.value = fullArea;
+}
+
+function calcArea(side) {
+    area = (3 / 2) * Math.pow(side, 2) * Math.sqrt(3)
+    return area
+}
